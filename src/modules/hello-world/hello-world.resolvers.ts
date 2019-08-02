3470becc6e756c2d7d0626aee540808a3212e420
Joris de Smedt <joris.desmedt@lightspeedhq.com>
2019-08-02 11:44:34 +0200

    Solution of exercise 001

--- src/modules/hello-world/hello-world.resolvers.ts
+++ src/modules/hello-world/hello-world.resolvers.ts
@@ -3,12 +3,20 @@ import { Resolver } from '../../types/graphql-utils.types';
 
 export interface HelloWorldResolvers extends IResolvers {
   Query: {
-    hello: Resolver<undefined, { greeter: string }, string>;
+    hello: Resolver<undefined, { greeter: string }, { greeter: string }>;
+  };
+  HelloWorld: {
+    at: Resolver<{ greeter: string }, undefined, string>;
   };
 }
 
 export const helloWorldresolvers: HelloWorldResolvers = {
   Query: {
-    hello: (_source, _args, _context) => 'World',
+    hello: (_source, args, _context) => ({
+      greeter: args.greeter,
+    }),
+  },
+  HelloWorld: {
+    at: (_source, _args, _context) => new Date().toLocaleString(),
   },
 };
--- src/modules/hello-world/hello-world.types.ts
+++ src/modules/hello-world/hello-world.types.ts
@@ -1,7 +1,12 @@
 import { gql } from 'apollo-server-core';
 
 export const helloWorldTypDefs = gql`
+  type HelloWorld {
+    greeter: String
+    at: String
+  }
+
   type Query {
-    hello: String
+    hello(greeter: String): HelloWorld
   }
 `;
