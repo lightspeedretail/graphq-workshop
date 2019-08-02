1b50b70d65f759332d5d1fa627b4603c6127e6b2
Joris de Smedt <joris.desmedt@lightspeedhq.com>
2019-08-02 12:04:49 +0200

    solution 004

--- src/modules/products/products.resolvers.ts
+++ src/modules/products/products.resolvers.ts
@@ -1,6 +1,6 @@
 import { IResolvers } from 'graphql-tools';
 import { Resolver } from '../../types/graphql-utils.types';
-import { Product } from './products.fixture';
+import { Product, ProductCategory } from './products.fixture';
 
 // uncomment these types when needed
 
@@ -8,6 +8,8 @@ export interface ProductResolvers extends IResolvers {
   Query: {
     products: Resolver<undefined, { name?: string }, Product[]>;
     product: Resolver<undefined, { id: string }, Product | undefined>;
+    categories: Resolver<undefined, { name?: string }, ProductCategory[]>;
+    category: Resolver<undefined, { id: string }, ProductCategory | undefined>;
   };
   Mutation: {
     addProduct: Resolver<undefined, { name: string; price: number; description?: string }, Product>;
@@ -17,6 +19,13 @@ export interface ProductResolvers extends IResolvers {
       Product
     >;
     deleteProduct: Resolver<undefined, { id: string }, string>;
+    addCategory: Resolver<undefined, { name: string; description?: string }, ProductCategory>;
+    updateCategory: Resolver<
+      undefined,
+      { id: string; name?: string; description?: string },
+      ProductCategory
+    >;
+    deleteCategory: Resolver<undefined, { id: string }, string>;
   };
 }
 
@@ -30,6 +39,14 @@ export const productResolvers: ProductResolvers = {
     },
     product: (_source, args, context) =>
       context.dataSources.productsDataSource.findProduct(args.id),
+    categories: (_source, args, context) => {
+      if (args.name) {
+        return context.dataSources.productsDataSource.getProductCategoriesByName(args.name);
+      }
+      return context.dataSources.productsDataSource.getProductCategories();
+    },
+    category: (_source, args, context) =>
+      context.dataSources.productsDataSource.findProductCategory(args.id),
   },
   Mutation: {
     addProduct: (_source, product, context) =>
@@ -41,6 +58,18 @@ export const productResolvers: ProductResolvers = {
       context.dataSources.productsDataSource.updateProduct(id, product),
     deleteProduct: (_source, { id }, context) => {
       context.dataSources.productsDataSource.deleteProduct(id);
+      return id;
+    },
+    addCategory: (_source, category, context) =>
+      context.dataSources.productsDataSource.addProductCategory({
+        description: '',
+        ...category,
+        productIds: [],
+      }),
+    updateCategory: (_source, { id, ...product }, context) =>
+      context.dataSources.productsDataSource.updateProductCategory(id, product),
+    deleteCategory: (_source, { id }, context) => {
+      context.dataSources.productsDataSource.deleteProductCategory(id);
       return id;
     },
   },
--- src/modules/products/products.types.ts
+++ src/modules/products/products.types.ts
@@ -8,14 +8,25 @@ export const productTypDefs = gql`
     price: Float
   }
 
+  type ProductCategory {
+    id: ID
+    name: String
+    description: String
+  }
+
   type Query {
     products(name: String): [Product!]!
     product(id: ID!): Product
+    categories(name: String): [ProductCategory!]!
+    category(id: ID!): ProductCategory
   }
 
   type Mutation {
     addProduct(name: String!, price: Float!, description: String): Product
     updateProduct(id: ID!, name: String, price: Float, description: String): Product
     deleteProduct(id: ID!): String
+    addCategory(name: String!, description: String): ProductCategory
+    updateCategory(id: ID!, name: String, description: String): ProductCategory
+    deleteCategory(id: ID!): String
   }
 `;
